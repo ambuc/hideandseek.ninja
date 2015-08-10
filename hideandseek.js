67f66831c2ca7bd5f86b3cc781ab851cfecb7737
ambuc <bucklnd2@illinois.edu>
2015-08-09 23:51:30 -0400

    v2, with data separation

--- hideandseek.js
+++ hideandseek.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
 
-    $('.scrollspy').scrollSpy();
-	$('.toc-wrapper').pushpin({ top: 0 });
+
+	$('.splash-text').html( 
+		_.template( $( "#splash_text_template" ).html() ) ( data ) 
+	);
+
+	$('.bases-table-text').html( 
+		_.template( $( "#bases_table_template" ).html() ) ( data ) 
+	);
+
+	$('.map-image').html( 
+		_.template( $( "#map_image_template" ).html() ) ( data ) 
+	);
+
+	$('.schedule-text').html( 
+		_.template( $( "#schedule_text_template" ).html() ) ( data ) 
+	);
+
+    $('.materialboxed').materialbox();
 	$('.parallax').parallax();
     $(".button-collapse").sideNav();
+
+	console.log('nice');
+
 
 });
 
---
+++ stockholm.js
@@ -0,0 +1,76 @@
+var data = {
+	"where" : {
+		"city" : "Stockholm",
+		"country" : "Sweden"		
+	},
+	"when" : {
+		"month" : "May",
+		"day" : "31",
+		"year" : "2015",
+		"time" : "Noon",
+	},
+	"big_picture" : "http://images.nationalgeographic.com/wpf/media-live/photos/000/025/cache/stockholm-old-town_2566_600x450.jpg",
+	"map" : "https://raw.githubusercontent.com/ambuc/hideandseek/master/map.png",
+	"zones" : {
+		"count" : 12,
+		"array" : [
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the triangle by the Stenbock Palaces in Riddarholmen",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the center of Stortorget in Gamla Stan",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the Kornhamnstorg public square in Gamla Stan",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "Strömparterren, the park by the Museum of Medieval Stockholm",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "meet at Fjäderholmslinjen, the fishing pier",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the western exit of the Gamla Stan metro, along Munkbrohamnen",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the Stockholm Strömkajen ferry dock",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the statue in Gustav Adolfs torg",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the docked ship along Västra Brobänken in Skeppsholmen",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the spider at Moderna Museet",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the statue of Charles XIII in Kungsträdgården (the skating Zones rink)",
+			},
+			{
+				"link" : "maps.google.com/location",
+				"desc" : "the main entrance to Kulturhuset in Sergels Torg"
+			}
+		]
+	},
+	"schedule" : {
+		"start_time" : "2",
+		"ampm" : "pm",
+		"before_duration" : "15",
+		"hiding_duration" : "10", //hiding+seeking+reset = 60, always
+		"seeking_duration" : "40",
+		"reset_duration" : "10" 
+	},
+	"meetup_location" : "Kungstradgarden"
+};
