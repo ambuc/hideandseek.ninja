cbc4f7bcc0bbb1ac8cb8824914a1d75a1cc7d35c
ambuc <bucklnd2@illinois.edu>
2015-08-25 10:06:42 -0500

    wow, great job :D

--- hideandseek.js
+++ hideandseek.js
@@ -6,19 +6,9 @@ var Player = Parse.Object.extend("Player", {
 	    'home'		:   null,
 	    'secret_code'	: 	null,
 	    'home_base'		: 	null,
-	    'team'			: 	null,
-	    'kills'		: 	null,
-	    'deaths'	: 	null,
-	    'kdr'		: 	null,
-	    'score'		: 	null,
+	    'team'			: 	null
 	}, 
 	initialize: function() {
-		this.set({
-			'kills' : 0,
-			'deaths': 1,
-			'kdr'	: 0,
-			'score' : 0
-		});
 	}
 });
 
@@ -31,60 +21,25 @@ var Find = Parse.Object.extend("Find", {
 	}
 });
 
-
 $(document).ready(function(){
 
 	Parse.$ = jQuery; //reassign jQuery, god knows why
 	Parse.initialize("oDrVT97pILfNP3PtXnzSwZD0vsgScEGdLmR0ZtBO", "d7fE3uszRBHCai7pLgSmuxEntLaUbIvNG5RKuruA");
 
-	$('section').hide();
-
-	var current_tab = "#section-about";
-	initialize_tabs();
-
-	go_to_tab(current_tab, false);
-
 	template_from_data( data );
 	start_materialize();
 
+	// tabs
+
+	var current_tab = "#section-score";
+	initialize_tabs();
+	go_to_tab(current_tab, false);
+
+	initialize_footer();
 	initialize_register();
 	initialize_scoring();
 
 });
-
-
-function initialize_tabs(){
-
-	$('ul.nav-tabs li a').click(function(){
-		go_to_tab($(this).attr('data-href'));
-	});
-
-	$('#signmeup').click(function(){
-		console.log('clicked register now');
-		go_to_tab('#section-register');
-	});
-
-}
-
-function go_to_tab(tab, isScroll){
-   if (typeof(isScroll)==='undefined') { isScroll = true; }
-
-	console.log(tab);
-	$('section').hide();
-	$(tab).show();
-	if(isScroll){
-		$(tab).scrollintoview({
-		    duration: 1700,
-		    direction: "vertical"
-		});		
-	}
-}
-
-function start_materialize(){
-    $('.materialboxed').materialbox();
-	$('.parallax').parallax();
-    $(".button-collapse").sideNav();
-}
 
 function template_from_data( city ){
 	$('.splash-text').html( 
@@ -102,6 +57,49 @@ function template_from_data( city ){
 	$('.schedule-text').html( 
 		_.template( $( "#schedule_text_template" ).html() ) ( city ) 
 	);
+}
+
+function start_materialize(){
+    $('.materialboxed').materialbox();
+	$('.parallax').parallax();
+    $(".button-collapse").sideNav();
+}
+
+// tabs
+
+function initialize_tabs(){
+	$('ul#row-tabs li a').click(function(){
+		go_to_tab($(this).attr('data-href'));
+	});
+
+	$('#signmeup').click(function(){
+		go_to_tab('#section-register');
+	});
+}
+
+function go_to_tab(tab, isScroll){
+   if (typeof(isScroll)==='undefined') { isScroll = true; }
+
+	$('section').addClass('hide');
+	$(tab).removeClass('hide');
+
+	if(isScroll){
+		$(tab).find('h1').next().next().scrollintoview({
+		    duration: 1700,
+		    direction: "vertical"
+		});		
+	}
+}
+
+function initialize_footer(){
+	var cities = ["sydney", "tokyo", "amsterdam", "london", "paris", "madrid", "nyc", "chicago", "stockholm", "copenhagen", "berlin", "sanfrancisco", "shanghai", "beijing", "istanbul", "mumbai", "cairo", "seoul", "bangkok", "tehran", "baghdad", "pyongyang"];
+
+    window.setInterval( function(){
+	    $("#cycle_city").fadeOut( 500, function(){
+		    $("#cycle_city").html(_.sample(cities));
+		    $("#cycle_city").fadeIn();    	
+	    });
+    }, 2400 );
 }
 
 // register
@@ -132,7 +130,7 @@ function initialize_register(){
 	});
 }
 
-function addCode(thisPlayer){
+function add_code(thisPlayer){
 	var email = thisPlayer.get('email');
 	var e = email.charAt(0).toUpperCase();
 	var secret_code = '';
@@ -147,12 +145,12 @@ function addCode(thisPlayer){
 
 			thisPlayer.set('secret_code', secret_code);
 			thisPlayer.save();
-			fireEmail(thisPlayer);
+			fire_email(thisPlayer);
 	  	}
 	});
 }
 
-function fireEmail(thisPlayer){
+function fire_email(thisPlayer){
 	var params = { 
 		'email' : thisPlayer.get('email'),
 		'first' : thisPlayer.get('first'),
@@ -166,11 +164,11 @@ function fireEmail(thisPlayer){
 	};
 	Parse.Cloud.run('fireEmail', params, {
 		success: function(msg) {
-			console.log(msg);
+			// console.log(msg);
 		// ratings should be 4.5
 		},
 		error: function(error) {
-			console.log(error);
+			// console.log(error);
 		}
 	});
 }
@@ -185,17 +183,17 @@ function register_user(){
 		'team'  	: (Math.random()>0.5)?'A':'B'
 	});
 
-	console.log(thisPlayer);
+	// console.log(thisPlayer);
 
 	thisPlayer.save({
 		success: function() {
-			addCode(thisPlayer);
+			add_code(thisPlayer);
 			show_registration(thisPlayer, false);
 			$('#registry-form').remove();
 		},
 		error: function(error) {
 			alert('Something went wrong, reload the page and try again.');
-			console.log(error);
+			// console.log(error);
 		}
 	});
 }
@@ -224,61 +222,86 @@ function show_registration(thisPlayer, isAlreadyRegistered){
 //scoring
 
 function initialize_scoring(){
-	var curr_player;
-	var counter = 3;
-	var codes = [];
 
 	$('#check').click(function(){
-		checkIdents($('#my_code').val().toUpperCase(),$('#my_base').val());
+		$('#i-scoring-login-icon').removeClass('animaed wobble');
+		check_idents($('#my_code').val().toUpperCase(),$('#my_base').val());
 	});
+
+	$('#add-fields').click(function(){
+		addField(2);
+	});
+
 }
 
-function checkIdents(code, base){
+function check_idents(code, base){
 	var query = new Parse.Query(Parse.Object.extend("Player"));
 	query.equalTo("secret_code", code);
 	query.find({
 		success: function(results){
-			console.log(results);
+			// console.log(results);
 			if(results.length>0){
 				if(results[0].get('home_base')==base){
-					verifyUser(results[0].get('first'));				
+					verify_user(results[0]);				
 				} else {
-					denyUser();
+					deny_user();
 				}
 			} else {
-				denyUser();
+				deny_user();
 			}
 		}
 	});
 }
 
-function denyUser(){
-	$('#sub-scoring-login-icon').html('announcement');
-	$('#sub-scoring-login-icon').removeClass('blue-text');
-	$('#sub-scoring-login-icon').addClass('red-text animated wobble');
+function deny_user(){
+	$('#i-scoring-login-icon').html('announcement');
+	$('#i-scoring-login-icon').removeClass('blue-text');
+	$('#i-scoring-login-icon').addClass('red-text animated wobble');
 
-	$('#sub-scoring-login-msg').html("Sorry, we can't seem to log you in. Try again?");
+	$('#h4-scoring-login-msg').html("Sorry, we can't seem to log you in. Try again?");
 	$('#my_code').val('');
 	$('#my_base').val('');
 
 	// alert('no');
 }
 
-function verifyUser(first){
-	$('#sub-scoring-login-icon').html('face');
-	$('#sub-scoring-login-icon').addClass('animated tada');
-	$('#sub-scoring-login-icon').toggleClass('blue-text green-text');
-	$('#sub-ident').remove();
+function verify_user(thisPlayer){
 
-	$('#sub-scoring-login-msg').html("Welcome back, <b class='green-text'>" + first + "</b>!");
+	$('#col-scoring-instructions h4').prepend("<small>Welcome back, <b class='green-text'>" + thisPlayer.get('first') + "</b>! ("+thisPlayer.get('secret_code')+")</small><br/>");
 
-	$('#sub-scoring-entry').removeClass('hide');
-	$('#sub-scoring-entry').addClass('animated bounceInRight');
+	$('#row-scoring-login').slideUp("normal", function() { 
+		$(this).remove(); 
+	} );
+
+	$('#row-scoring-entry').removeClass('hide');
+	$('#row-scoring-entry').addClass('animated fadeInUp');
+}
+
+function addField(n){
+	for(var i = 0; i < n; i++){
+		$('.fields').append("<div class='field input-field'> <input class='find' placeholder='secret code' maxlength=4 type='text' class='validate'> </div>");
+		$('.fields-row input').unbind();
+		$('.fields-row input').keyup(function(){
+			checkCode($(this));
+		});
+	}
+}
+
+function loadCodes(){
+	var query = new Parse.Query(Parse.Object.extend("Player"));
+	query.find({
+		success: function(results){
+			var codes = _.map(results, function(res){return res.get('secret_code')});
+		},
+		error: function(bar){
+			alert('Please reload the page, something went wrong');
+		}
+	});
 }
 
 
 
-//general use functions
+//general
 
 function validate_user_email(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -301,6 +324,133 @@ function pad_string(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
+	
+
+
+
+
+
+
+//todo
+// shouldn't be able to find yourself
+// shouldn't be able to add duplicates
+// scores need to auto-update
+
+// add rankings page
+
+// function checkCode(foo){
+// 	if(foo.val().length!=4){return;}
+// 	var val = foo.val().toUpperCase();
+// 	var id = foo.parent().attr('data-id');
+
+// 	foo.parent().children('i').remove();
+// 	if( _.indexOf(codes, val) != -1){
+// 		foo.attr('data-valid', 'true');
+// 		$("<i class='material-icons green-text prefix animated rubberBand'> thumb_up </i>").insertBefore(foo);
+// 	} else {
+// 		foo.attr('data-valid', 'false');
+// 		$("<i class='material-icons orange-text prefix animated shake badcode'> thumb_down </i>").insertBefore(foo);
+// 	}
+
+// 	$("i.badcode").unbind();
+// 	$("i.badcode").mouseover(function(){
+// 		$(this).html('remove_circle_underline');
+// 		$(this).toggleClass('red-text orange-text');
+// 	});
+// 	$("i.badcode").mouseout(function(){
+// 		$(this).html('thumb_down');
+// 		$(this).toggleClass('red-text orange-text');
+// 	});
+// 	$("i.badcode").click(function(){
+// 		$(this).parent().remove();
+// 	});
+// }
+
+// function checkLast(foo){
+// 	if(_.isUndefined(foo.next()[0])){
+// 		add_field(1);
+// 	}
+// 	$('.fields-row .field').unbind();
+// 	$('.fields-row .field').keydown(function(){
+// 		checkLast($(this));
+// 	});
+// }
+
+// //PHASE 3
+
+// function pushFinds(){
+// 	goodcodes = getGoodCodes();
+
+// 	//push each find
+// 	var seeker = $('#my_code').val().toUpperCase();
+// 	for(var i = 0; i < goodcodes.length; i++){
+
+// 		pushFind( seeker, goodcodes[i] );
+// 		addKill();
+// 		addDeath( goodcodes[i] );
+// 	}
+// }
+
+// function addKill(){
+// 	player.set('kills', player.get('kills')+1);
+// 	player.save();
+// }
+
+// function addDeath(code){
+// 	var query = new Parse.Query(Parse.Object.extend("Player"));
+// 	query.equalTo("secret_code", code );
+// 	query.find({
+// 	  	success: function(results) {
+// 	  		var player = results[0];
+// 			player.set('deaths', player.get('deaths')+1);
+// 			player.save();
+// 	  	}
+// 	});
+// }
+
+// function pushFind(seeker, hider){
+// 	var newFind = new Find({
+// 		'seeker' : seeker,
+// 		'hider' : hider
+// 	});
+
+// 	console.log(newFind);
+
+// 	newFind.save({
+// 		success: function() {
+// 			console.log('saved successfully');
+// 		},
+// 		error: function(error) {
+// 			console.log(error);
+// 		}
+// 	});
+// }
+
+// function getGoodCodes(){
+// 	var trues = $('.find[data-valid=true]');
+
+// 	var goodcodes = [];
+
+// 	for(var i = 0; i < trues.length; i++){
+// 		goodcodes.push($(trues[i]).val().toUpperCase());
+// 	}
+
+// 	return goodcodes;
+// }
+
+// //PHASE 4
+
+// function displayStats(){
+// 	$('.fields-row').addClass('hide');
+// 	$('.stats-row').removeClass('hide');
+// 	getStats();
+// }
+
+// function getStats(){
+// 	$('.deaths').html(player.get('deaths'));
+// 	$('.kills').html(player.get('kills'));
+// 	$('.score').html(player.get('score'));
+// }
 
 
 
